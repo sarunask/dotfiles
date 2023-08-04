83aba0146f3eafbd498bb715b72a031a752b37c4
Sarunas Krisciukaitis <sarunas.krisciukaitis@gmail.com>
2023-08-04 09:22:50 +0100

    Initial after https://www.youtube.com/watch?v=w7i4amO_zaE&list=RDLVX6AR2RMB5tE&index=22

---
+++ config/nvim/after/plugin/telescope.lua
@@ -0,0 +1,6 @@
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
+vim.keymap.set('n', '<C-s>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ss', function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+end)
---
+++ config/nvim/lua/sarunas/init.lua
@@ -0,0 +1,5 @@
+-- To use packer I will need to load this file with :so and then do all funcs
+-- require("sarunas.packer")
+require("sarunas.set")
+require("sarunas.remap")
+
